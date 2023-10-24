962aa57ed4b6ec395b24822a15d3ce538bd9e2fa
Laparevich Daniil <danik.laparevich@bk.ru>
2023-10-25 01:57:16 +0300

    initial commit

---
+++ src/main/java/com/example/springserviceforcvswaggerdocker/model/Specialization.java
@@ -0,0 +1,4 @@
+package com.example.springserviceforcvswaggerdocker.model;
+
+public class Specialization {
+}
---
+++ src/test/java/com/example/springserviceforcvswaggerdocker/SpringServiceForCvSwaggerDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springserviceforcvswaggerdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceForCvSwaggerDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
